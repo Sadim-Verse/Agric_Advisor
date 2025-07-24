d114d14e9ea80ae7a2982bb0de27399e39dff82b
Sadim <148269014+Sadim-Verse@users.noreply.github.com>
2025-07-24 06:19:40 +0100

    Added files
    Added the compressed model file and other files via upload

---
+++ app.py
@@ -0,0 +1,144 @@
+import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import time
+
+from yield_ import get_countries, get_crops, get_min_max, crop_classification
+from huggingface_hub import InferenceApi
+
+# # Load the required files
+# inference = InferenceApi(
+#     repo_id = "Sad1m/agric_advisor",
+#     token= "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"
+# )
+
+model = joblib.load(r"C:\Users\HP\Projects\Python_for_ML\Regression\Agric_advisor\rfc_model_c.joblib")
+le_co = joblib.load(r"C:\Users\HP\Projects\Python_for_ML\Regression\Agric_advisor\country_le")
+le_cr = joblib.load(r"C:\Users\HP\Projects\Python_for_ML\Regression\Agric_advisor\crop_le")
+
+st.set_page_config(
+    page_title="An Agric Advisor",
+    layout="wide"
+)
+    
+# Change the font size and style of the page
+st.markdown("""
+            <style>
+            html, body {
+            font-size: 20px;
+            font-family: 'courier', 'sans-serif';
+            }
+            </style>
+            """, unsafe_allow_html=True)
+
+
+
+with st.sidebar:
+    st.header("🌾 Crop Yield Predictor")
+    st.write(
+        """
+        This app uses a regression model to predict a crop’s **annual yield** based on:
+        - **Country** where it’s planted  
+        - **Crop** 
+        - Average annual **temperature**
+        - Total annual **rainfall**
+        - Average annual **pesticide use**
+        """
+    )
+    st.info(
+        """
+        To get an accurate prediction, please make sure *all* inputs are provided correctly.
+        """
+    )
+
+# Interface for the user to get input the features
+st.title("Agric Advisor")
+st.markdown("""<html>
+            <i>Planning your next big harvest? Agric Advisor provides data-driven predictions and tailored suggestions
+             for large-scale farmers looking to maximize crop yields and streamline operations.</i>
+            <p></p>
+            </html>
+            """, unsafe_allow_html=True)
+st.write("#")
+
+
+# Get the country names
+country_names = get_countries()
+country_name = st.selectbox(
+    "**Where are you planting your crops?**", country_names,
+    index=None,
+    placeholder="Select your country...",
+    accept_new_options=False,
+    )
+st.write(f"You Selected: **{country_name}**")
+
+
+st.write("--------------------------")
+
+# Get the Crop names
+crop_names = get_crops()
+crop_name = st.selectbox("**What crop are you planting?**", crop_names, 
+                      index=None, placeholder="Select your crop...")
+st.write(f"You Selected: {crop_name}")
+
+
+st.write("--------------------------")
+
+# Enter the numerical values
+avg_rainfall = st.number_input("Average Annual Rainfall: ", 
+                               min_value=get_min_max(1, 0, 'rain'), 
+                               max_value=get_min_max(0, 1, 'rain'),
+                               value=None, placeholder=0.00)
+
+st.write("--")
+pesticide_use = st.number_input("Amount of pesticides used: ", 
+                                min_value=get_min_max(1, 0, 'pest'),
+                                max_value=get_min_max(0, 1, 'pest'),
+                                value=None, placeholder=0.00)
+
+st.write("--")
+avg_temp = st.number_input("Average Weather Temperature: ",
+                           min_value=get_min_max(1, 0, 'temp'),
+                           max_value=get_min_max(0, 1, 'temp'),
+                           value=None, placeholder=0.00)
+
+
+country_name = le_co.transform(pd.Series(data=country_name))
+crop_name = le_cr.transform(pd.Series(data=crop_name))
+
+# Model prediction
+
+predict_button = st.button("Predict Annual Crop Yield")
+predicted_yield = 0
+
+# if predict_button and 
+
+if predict_button:
+    features = pd.DataFrame({'Country': country_name, 'Crop': crop_name, 'Average_annual_rainfall(mm)': avg_rainfall,
+       'Pesticides_used(tonnes)': pesticide_use, 'Average_temperature(degree C)': avg_temp})
+    
+    predicted_yield = model.predict(features)[0]
+    crop_name = le_cr.inverse_transform(crop_name)[0].lower()
+    st.write(f"Predicted yield for {crop_name} is {predicted_yield} hectogram per hectare")
+
+# Logic for the suggestions to the user
+
+if predicted_yield != 0:
+    crop_classification(crop_name, predicted_yield)
+
+
+
+from huggingface_hub import HfApi
+from huggingface_hub import upload_file
+
+# api = HfApi()
+# api.create_repo(repo_id="agric_advisor", private=True)
+
+# api.upload_folder(
+#     repo_id = "Sad1m/agric_advisor",
+#     folder_path= r"C:\Users\HP\Projects\Python_for_ML\Regression\Agric_advisor",
+#     path_in_repo="",
+#     repo_type= "space"
+# )
---
+++ yield_.py
@@ -0,0 +1,155 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+import time
+
+yield_data = pd.read_csv(r"C:\Users\HP\Projects\Python_for_ML\Regression\Agric_advisor\new_yield_df.csv")
+
+yield_data
+
+def get_countries():
+    return tuple(yield_data['Country'].unique())
+
+def get_crops():
+    return tuple(yield_data['Crop'].unique())
+
+def get_min_max(min, max, col):
+    if( (min > 0) and (max == 0) ):
+        if(col == 'rain'):
+            return yield_data['Average_annual_rainfall(mm)'].min()
+        elif(col == 'pest'):
+            return yield_data['Pesticides_used(tonnes)'].min()
+        elif(col == 'temp'):
+            return yield_data['Average_temperature(degree C)'].min()
+
+    elif( (min < 0) and (max == 0) ):
+        if(col == 'rain'):
+            return yield_data['Average_annual_rainfall(mm)'].max()
+        elif(col == 'pest'):
+            return yield_data['Pesticides_used(tonnes)'].max()
+        elif(col == 'temp'):
+            return yield_data['Average_temperature(degree C)'].max()
+
+crop_type = ""  
+# Classify the crops into different categories:
+def crop_classification(crop_name, predicted_yield):
+    global crop_type 
+
+    cereal_crops = ['Maize', 'Rice, paddy', 'Sorghum', 'Wheat']
+    root_crops = ['Potatoes', 'Yams']
+    tuber_crops = ['Cassava', 'Sweet potatoes']
+    legume_crops = ['Soybeans']
+    fruit_crops = ['Plantains and others']
+
+    crop = crop_name.capitalize()
+
+    if(crop in cereal_crops): crop_type = "cereal"
+    elif(crop in root_crops): crop_type = "root"
+    elif(crop in tuber_crops): crop_type = "tuber"
+    elif(crop in legume_crops): crop_type = "legume"
+    elif(crop in fruit_crops): crop_type = "fruit"
+    
+    with st.spinner("Loading..."):
+        time.sleep(1.5)
+    crop_specs(crop_name, predicted_yield)
+
+specs = {
+    "Rainfall": {
+        "Poor": "If your area receives less rain than your crop needs,"
+        " consider adding water-saving methods like zai pits, contour ridges, or mulch to retain moisture.",
+
+        "Average": "If the rains are unpredictable, wait until you’ve had two steady rainfall events before planting."
+        " You can also shape shallow ridges in your field to help more water soak in when it does rain.",
+
+        "Good": "If rainfall differs by at least 15% across your field,"
+        " install multiple rain gauges and apply extra water only where it's needed.",
+
+        "Excellent": "If your soil isn’t holding enough water, try deep ripping,"
+        " apply organic mulch, or grow cover crops to improve retention."
+    },
+
+    "Temperature": {
+        "Poor": "Does the temperature fall outside the crop’s comfort zone?"
+        " Shift planting dates or switch to stress-tolerant varieties.",
+
+        "Average": "If your area stays in the 30–35 °C range during early growth stages, "
+        "apply foliar nutrients like zinc and boron to support healthy grain formation even under heat stress.",
+
+        "Good": "If you’re seeing occasional bursts of heat or cold, "
+        "sign up for local weather alerts. Use these to time biostimulant or growth regulator sprays more effectively.",
+
+        "Excellent": "If your crops already handle tough weather well, "
+        "run small on-farm trials with new, stress-tolerant varieties to future-proof your production."
+    },
+
+    "Pesticide": {
+        "Poor": "Are you scouting for pests/diseases regularly? If you're not inspecting your field regularly,"
+        " start doing weekly checks. And make sure your spraying equipment is properly calibrated before applying any treatments.",
+
+        "Average": "If you’re using a fixed spray calendar, it's time to switch. "
+        "Spray only when pest thresholds are reached, and use seed treatments at planting to reduce early-season outbreaks.",
+
+        "Good": "If you notice pest or disease hotspots,"
+        " use traps or field imagery to find those zones and treat them specifically—no need to spray the entire field.",
+
+        "Excellent": "If you’re spraying more than recommended for your crop type, consider forming a local IPM (Integrated Pest Management) "
+        "group to share natural controls and remove the least effective treatments from your routine."
+    }
+}
+
+def crop_specs(crop_name, predicted_yield):
+
+    if(crop_name == "rice, paddy"):
+        crop_name = "rice(paddy)"
+    st.write(f"{crop_name.capitalize()} is a {crop_type} crop.")
+
+    if predicted_yield < 20000:
+        st.markdown("""<html>
+            <p>Your annual crop yield is <b>low.</b> <i>Here are a few things worth checking:</i>
+                    </p>
+            <p></p>
+            </html>
+            """, unsafe_allow_html=True)
+        my_list = [specs["Rainfall"]["Poor"], specs["Temperature"]["Poor"], specs["Pesticide"]["Poor"]]
+        markdown_list = "\n".join([f"- {item}" for item in my_list])
+        st.markdown(markdown_list)
+        
+        
+
+    elif predicted_yield < 40000:
+        st.markdown("""<html>
+            <p>Your yield is average—<b>not bad,</b> <i>but there's room to grow. Try these steps to push closer to good or excellent: </i>
+                    </p>
+            <p></p>
+            </html>
+            """, unsafe_allow_html=True)
+        my_list = [specs["Rainfall"]["Average"], specs["Temperature"]["Average"], specs["Pesticide"]["Average"]]
+        markdown_list = "\n".join([f"- {item}" for item in my_list])
+        st.markdown(markdown_list)
+
+    
+    elif predicted_yield < 60000:
+        st.markdown("""<html>
+            <p>Your crop yield is good—<b>well done!</b>! <i>To improve even further, consider the following:   </i>
+                    </p>
+            <p></p>
+            </html>
+            """, unsafe_allow_html=True)
+        my_list = [specs["Rainfall"]["Good"], specs["Temperature"]["Good"], specs["Pesticide"]["Good"]]
+        markdown_list = "\n".join([f"- {item}" for item in my_list])
+        st.markdown(markdown_list)
+    
+
+    elif predicted_yield >= 60000:
+        st.markdown("""<html>
+            <p>Your annual crop yield is excellent—<b>great job!</b> <i>But even high-performing farms have room to level up:</i>
+                    </p>
+            <p></p>
+            </html>
+            """, unsafe_allow_html=True)
+        my_list = [specs["Rainfall"]["Excellent"], specs["Temperature"]["Excellent"], specs["Pesticide"]["Excellent"]]
+        markdown_list = "\n".join([f"- {item}" for item in my_list])
+        st.markdown(markdown_list)
+
+
